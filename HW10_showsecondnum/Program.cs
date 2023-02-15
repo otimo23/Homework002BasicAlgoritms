fe03cfd38fd0823b3d1e03f3749f2c09dd3ca658
olga Timoshenko <otimoshenko55@gmail.com>
2023-02-15 09:41:26 +0100

    на вход трёхзначное числ и на выходе показывает вторую цифру

---
+++ HW10_showsecondnum/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+int number = new Random().Next(100, 1000);
+int numberA = number / 10;
+int numberB = numberA % 10;
+
+Console.WriteLine($"{number} -> {numberB}"!);
+
+
+
+/*Напишите программу, которая принимает на вход трёхзначное число
+ и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8*/
