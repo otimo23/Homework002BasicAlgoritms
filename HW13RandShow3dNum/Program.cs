4d8be82f8e3784c09a9bb6a1c57fbc8c559886c3
olga Timoshenko <otimoshenko55@gmail.com>
2023-02-15 11:15:12 +0100

    выводит третью цифру заданного числа или сообщает, что ее нет

---
+++ HW13RandShow3dNum/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+int num = new Random().Next(0, 1000);
+if(num > 99)
+{
+int thirdnumber = num % 10;
+Console.WriteLine($"Third number of {num} is {thirdnumber}");
+}
+else
+{
+Console.WriteLine($"Third number of {num} does not exists");
+}
+
+
+
+
+
+/*Напишите программу, которая выводит третью цифру заданного числа
+ или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет*/
