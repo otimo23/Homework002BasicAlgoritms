73d8e6c61bbd6c07778e73532593318c94a11128
olga Timoshenko <otimoshenko55@gmail.com>
2023-02-15 10:27:12 +0100

    рандомное выходной или нет

---
+++ HW15vihodnoy/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+int day = new Random().Next(1, 8);
+if (day == 6 || day == 7)
+{
+    Console.WriteLine($"Day {day} is weekend!");
+}
+else
+{
+    Console.WriteLine($"Day {day} is weekday:(");
+}
+
